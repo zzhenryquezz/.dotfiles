c87545c3c172d81417a598e2abe6a6c7013f85ad
Henrique de Oliveira <henriqueoliwork@gmail.com>
2024-11-02 16:23:17 -0300

    feat: update 2024-11-02

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,30 +1,3 @@
-local numbertoggle = vim.api.nvim_create_augroup("numbertoggle", {})
-
-vim.api.nvim_create_autocmd(
-    { "InsertLeave" },
-    {
-        group = numbertoggle,
-        callback = function()
-            if vim.opt.number and vim.api.nvim_get_mode() ~= "i" then
-                vim.opt.relativenumber = true
-            end
-        end,
-    }
-)
-
-vim.api.nvim_create_autocmd(
-    { "InsertEnter" },
-    {
-        group = numbertoggle,
-        callback = function()
-            if vim.opt.number then
-                vim.opt.relativenumber = false
-                vim.cmd("redraw")
-            end
-        end,
-    }
-)
-
 require("config.lazy")
 require("options")
 require("keymaps")
--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -4,4 +4,9 @@ vim.keymap.set("i", "<c-s>", '<Esc>:w<cr>')
 vim.keymap.set("n", "<c-d>", '<c-d>zz')
 vim.keymap.set("n", "<c-s>", ':w<cr>')
 vim.keymap.set("n", "<c-u>", '<c-u>zz')
-vim.keymap.set("n", "p", 'p :silent! %s/\r//g<cr>')
+
+
+-- toggle relative numbers
+vim.keymap.set("n", "<leader>rn", function()
+    vim.opt.relativenumber = not vim.opt.relativenumber:get()
+end, { noremap = true, silent = true })
