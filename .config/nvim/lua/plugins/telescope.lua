ca2cf6bbd74a7dc306cf18b57585d1b76ce4537f
Henrique <henrique@sidekick-coder.com>
2025-01-01 12:58:31 -0300

    fix: not ignoring some files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,65 +1,70 @@
 return {
-    {
-        "nvim-telescope/telescope-file-browser.nvim",
-        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-        config = function()
-            local builtin = require("telescope.builtin")
+	{
+		"nvim-telescope/telescope-file-browser.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
 
-            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-        end,
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            local telescope = require("telescope")
-            local telescopeConfig = require("telescope.config")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			local telescope = require("telescope")
+			local telescopeConfig = require("telescope.config")
 
-            local vimgrep_arguments = { table.unpack(telescopeConfig.values.vimgrep_arguments) }
+			if not table.unpack then
+				table.unpack = unpack
+			end
 
-            table.insert(vimgrep_arguments, "--hidden")
-            table.insert(vimgrep_arguments, "--glob")
-            table.insert(vimgrep_arguments, "!**/.git/*")
+			local vimgrep_arguments = { table.unpack(telescopeConfig.values.vimgrep_arguments) }
 
-            telescope.setup({
-                defaults = {
-                    vimgrep_arguments = vimgrep_arguments,
-                    mappings = {
-                        n = {
-                            ["q"] = require("telescope.actions").close,
-                            ["d"] = require("telescope.actions").delete_buffer,
-                        },
-                    }, 
-                },
-                pickers = {
-                    find_files = {
-                        find_command = {
-                            "rg",
-                            "--files",
-                            "--hidden",
-                            "--no-ignore",
-                            "--glob",
-                            "!**/.git/*",
-                        }
-                    },
-                    buffers = {},
-                    grep_string = {
-                        additional_args = { "--hidden" }
-                    },
-                    live_grep = {
-                        additional_args = { "--hidden" }
-                    },
-                },
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-            })
+			table.insert(vimgrep_arguments, "--hidden")
+			table.insert(vimgrep_arguments, "--glob")
+            -- ignore .git and node_modules
+			table.insert(vimgrep_arguments, "!{**/.git/*,**/node_modules/*}")
 
-            require("telescope").load_extension("ui-select")
-        end,
-    },
+			telescope.setup({
+				defaults = {
+					vimgrep_arguments = vimgrep_arguments,
+					mappings = {
+						n = {
+							["q"] = require("telescope.actions").close,
+							["d"] = require("telescope.actions").delete_buffer,
+						},
+					},
+				},
+				pickers = {
+					find_files = {
+						find_command = {
+							"rg",
+							"--files",
+							"--hidden",
+							"--no-ignore",
+							"--glob",
+                            "!{**/.git/*,**/node_modules/*}",
+						},
+					},
+					buffers = {},
+					grep_string = {
+						additional_args = { "--hidden" },
+					},
+					live_grep = {
+						additional_args = { "--hidden" },
+					},
+				},
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
