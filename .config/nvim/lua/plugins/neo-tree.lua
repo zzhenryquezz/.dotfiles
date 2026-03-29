d3a093e5b45cd1885659d9c20a57c9820042649d
Henrique <henrique@sidekick-coder.com>
2026-03-29 12:37:23 -0300

    refactor: better neo-tree yank paths

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,92 +1,99 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		local neotree = require("neo-tree")
-		local components = require("neo-tree.sources.common.components")
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        local neotree = require("neo-tree")
+        local components = require("neo-tree.sources.common.components")
 
-		neotree.setup({
-			close_if_last_window = true,
-			autoselect_one = true,
-			window = {
-				position = "current",
-				mappings = {
-					["Y"] = function(state)
-						-- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
-						-- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
-						local node = state.tree:get_node()
-						local filepath = node:get_id()
-						local filename = node.name
-						local modify = vim.fn.fnamemodify
+        local function copy_selector(state, selector)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local filename = node.name
+            local modify = vim.fn.fnamemodify
 
-						local results = {
-							filepath,
-							modify(filepath, ":."),
-							modify(filepath, ":~"),
-							filename,
-							modify(filename, ":r"),
-							modify(filename, ":e"),
-						}
+            local results = {
+                absolute = filepath,
+                relative = modify(filepath, ":."),
+                home = modify(filepath, ":~"),
+                filename = filename,
+                name_no_ext = modify(filename, ":r"),
+                extension = modify(filename, ":e"),
+            }
 
-						-- absolute path to clipboard
-						local i = vim.fn.inputlist({
-							"Choose to copy to clipboard:",
-							"1. Absolute path: " .. results[1],
-							"2. Path relative to CWD: " .. results[2],
-							"3. Path relative to HOME: " .. results[3],
-							"4. Filename: " .. results[4],
-							"5. Filename without extension: " .. results[5],
-							"6. Extension of the filename: " .. results[6],
-						})
+            local result = results[selector]
 
-						if i > 0 then
-							local result = results[i]
-							if not result then
-								return print("Invalid choice: " .. i)
-							end
-							vim.fn.setreg('+', result)
-							vim.notify("Copied: " .. result)
-						end
-					end,
-				},
-			},
-			filesystem = {
-				follow_current_file = {
-					enabled = true,
-					leave_dirs_open = true,
-				},
-				components = {
-					name = function(config, node, state)
-						local name = components.name(config, node, state)
-						if node:get_depth() == 1 then
-							name.text = vim.fs.basename(vim.loop.cwd() or "")
-						end
-						return name
-					end,
-				},
-				filtered_items = {
-					hide_dotfiles = false,
-					visible = true,
-					never_show = {
-						".git",
-					},
-				},
-			},
-			event_handlers = {
-				{
-					event = "file_open_requested",
-					handler = function()
-						vim.cmd("Neotree close")
-					end,
-				},
-			},
-		})
+            if result then
+                vim.fn.setreg("+", result) -- system clipboard 
+                vim.fn.setreg('"', result) -- default register
+                vim.notify("Copied: " .. result)
+            else
+                vim.notify("Invalid selector: " .. selector, vim.log.levels.WARN)
+            end
+        end
 
-		vim.keymap.set("n", "<C-n>", ":Neotree reveal<CR>")
-	end,
+        neotree.setup({
+            close_if_last_window = true,
+            autoselect_one = true,
+            window = {
+                position = "current",
+                mappings = {
+                    ["<leader>pa"] = function(state)
+                        copy_selector(state, "absolute")
+                    end,
+                    ["<leader>pr"] = function(state)
+                        copy_selector(state, "relative")
+                    end,
+                    ["<leader>ph"] = function(state)
+                        copy_selector(state, "home")
+                    end,
+                    ["<leader>pf"] = function(state)
+                        copy_selector(state, "filename")
+                    end,
+                    ["<leader>pn"] = function(state)
+                        copy_selector(state, "name_no_ext")
+                    end,
+                    ["<leader>pe"] = function(state)
+                        copy_selector(state, "extension")
+                    end,
+                },
+            },
+            filesystem = {
+                follow_current_file = {
+                    enabled = true,
+                    leave_dirs_open = true,
+                },
+                components = {
+                    name = function(config, node, state)
+                        local name = components.name(config, node, state)
+                        if node:get_depth() == 1 then
+                            name.text = vim.fs.basename(vim.loop.cwd() or "")
+                        end
+                        return name
+                    end,
+                },
+                filtered_items = {
+                    hide_dotfiles = false,
+                    visible = true,
+                    never_show = {
+                        ".git",
+                    },
+                },
+            },
+            event_handlers = {
+                {
+                    event = "file_open_requested",
+                    handler = function()
+                        vim.cmd("Neotree close")
+                    end,
+                },
+            },
+        })
+
+        vim.keymap.set("n", "<C-n>", ":Neotree reveal<CR>")
+    end,
 }
