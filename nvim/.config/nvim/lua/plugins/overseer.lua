66fcd0a44862427ba156bb7af29bb7534713f069
Henrique <henrique@sidekick-coder.com>
2026-08-10 05:46:24 -0300

    refactor: remove overseer keybind

--- nvim/.config/nvim/lua/plugins/overseer.lua
+++ nvim/.config/nvim/lua/plugins/overseer.lua
@@ -4,14 +4,5 @@ return {
     local overseer = require("overseer")
 
     overseer.setup(opts)
-
-    vim.keymap.set("n", "<leader>or", function()
-        -- call :OverseerRun 
-        vim.cmd("OverseerRun")
-    end)
-
-    vim.keymap.set("n", "<leader>o", function()
-        overseer.toggle()
-    end)
   end,
 }
