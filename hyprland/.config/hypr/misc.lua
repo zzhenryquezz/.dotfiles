a08af7abb8653b20b7cc4b4c922062e0d344a932
Henrique <henrique@sidekick-coder.com>
2026-08-24 17:35:23 -0300

    refactor(hypr): replace waybar with qs

--- hyprland/.config/hypr/misc.lua
+++ hyprland/.config/hypr/misc.lua
@@ -29,7 +29,7 @@ hl.config({
 })
 
 hl.on("hyprland.start", function()
-    hl.exec_cmd("waybar")
+    hl.exec_cmd("qs")
     hl.exec_cmd("hyprpaper")
     hl.exec_cmd("hypridle")
     hl.exec_cmd("sysmctl --user start hyprpolkitagent")
