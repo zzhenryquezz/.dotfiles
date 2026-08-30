eaaa06c5d905c3fda0d3cd23514ed91664eaddf0
Henrique <henrique@sidekick-coder.com>
2026-08-30 08:11:18 -0300

    refactor(hypr): update borders

--- hyprland/.config/hypr/look.lua
+++ hyprland/.config/hypr/look.lua
@@ -25,7 +25,7 @@ hl.config({
     },
 
     decoration = {
-        rounding         = 4,
+        rounding         = 16,
         rounding_power   = 2,
 
         -- Change transparency of focused and unfocused windows
